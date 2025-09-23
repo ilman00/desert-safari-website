645538864d49f216132bcd7c54e7160548cf82bf
Ilman <ilmanalisher896@gmail.com>
2025-09-23 05:57:16 +0500

    Forgot dbConnect

--- src/app/api/blogs/[slug]/route.js
+++ src/app/api/blogs/[slug]/route.js
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
-import { dbConnect } from "@/lib/mongodb";
+import { dbConnect } from "@/lib/dbConnect";
 import Blog from "@/models/Blogs";
 
 export async function GET(req, { params }) {
